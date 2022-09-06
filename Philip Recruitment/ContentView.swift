877eec36cc20a1a85a582d83995f491d88f5da5a
JonathanAndrei12 <jonathanandreiwibawa@gmail.com>
2022-09-06 22:26:18 +0700

    Initial Commit

---
+++ Philip Recruitment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Philip Recruitment
+//
+//  Created by Jonathan Andrei on 06/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Philip Recruitment/Philip_RecruitmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Philip_RecruitmentApp.swift
+//  Philip Recruitment
+//
+//  Created by Jonathan Andrei on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Philip_RecruitmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
